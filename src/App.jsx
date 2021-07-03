2fa0d0aaba9f6cf8cc0701d6cd27aaf5ac867b79
Antonella Mizzi <antonella9mizzi@gmail.com>
2021-07-03 14:11:52 +0200

    Creo css file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  console.log()
+
+  return (
+    <div className="App">
+      <NavBar nombreDeLaTienda="Flora" subtitulo="store"/>
+      <h1>hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
