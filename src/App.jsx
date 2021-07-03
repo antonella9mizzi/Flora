c4e1e38816d045d259dac1c71c2c35bbfe107b2e
Antonella Mizzi <antonella9mizzi@gmail.com>
2021-07-03 14:13:40 +0200

    Aplico estilos en Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar nombreDeLaTienda="Flora" subtitulo="store"/>
-      <h1>hola mundo</h1>
+      <h1>Hola mundo</h1>
     </div>
   );
 }
