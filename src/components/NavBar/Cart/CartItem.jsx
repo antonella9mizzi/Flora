0f2d2630a10e800483c3435d3335ea6a05a5ca1e
Antonella <contactamizzi@gmail.com>
2021-09-07 14:49:09 +0200

    Clear cart solved

---
+++ src/components/NavBar/Cart/CartItem.jsx
@@ -0,0 +1,26 @@
+
+import React, {useContext} from 'react';
+import { CartContext } from '../../../context/CartContext';
+import CartCSS from './Cart.module.css';
+
+ const  CartItem = ({itemReceived}) => {
+    
+    const {removeItem} = useContext(CartContext);
+    function deleteItem(){
+        removeItem(itemReceived);
+    }
+    return <div className='cart-item'>
+         <div key={itemReceived.id} className={CartCSS.itemContainer}>
+                                <img src={itemReceived.img} alt={itemReceived.name} className={CartCSS.itemImg}/>
+                                <div className={CartCSS.texts}>
+                                    <h2>{itemReceived.name}</h2>
+                                    <h5>Total: ${(itemReceived.quantity)*(itemReceived.price)} </h5>
+                                
+                                </div>
+                                <div onClick={()=>deleteItem(itemReceived)} className={CartCSS.delete}>
+                                    <img src="../../imgs/DeleteIcon.png" alt="Delete"/>
+                                </div> 
+                            </div>
+    </div>
+}
+export default CartItem;
--- src/components/NavBar/Cart/CartWidget.jsx
+++ src/components/NavBar/Cart/CartWidget.jsx
@@ -2,45 +2,38 @@ import React, {useContext} from'react';
 import { CartContext } from '../../../context/CartContext';
 import { Link } from 'react-router-dom';
 import CartCSS from './Cart.module.css'
+import CartItem from './CartItem';
 import Form from './Form/Form';
+
 const CartWidget = () => {
 
-    const {cartItems,removeItem} = useContext(CartContext);
-    const TotalPrice = cartItems.reduce((a,c)=>a + c.price * c.quantity,0);
+    const {cartItems, cleanCart, totalPrice} = useContext(CartContext);
+    const costo = cartItems.map(item => item.precio * item.cantidad);
+    
+
     return (
         <div className={CartCSS.container}>
-            {!cartItems.length
+            {cartItems.length > 0
                 ? <div className={CartCSS.container}>
+                    <h2>Tienes {cartItems.length} productos en tu carrito</h2>
+                    <div>
+                        {cartItems.map(itemReceived => <CartItem itemReceived={itemReceived} key={itemReceived.id}/> )} 
+                    </div> 
+                    <div>
+                        <h5>Total ${totalPrice}</h5>
+                    </div>
+                    <div>
+                        <button onClick={cleanCart}>Vaciar Carrito</button>                
+                    </div>
+                    <Form cartItems={cartItems}/>        
+                </div>
+                :
+                <div>
                     <h2>No tienes productos en tu carrito</h2>
                     <Link to="/">
                         <button className={CartCSS.btn}>Volver al inicio</button>
                     </Link>
                 </div>
-                : 
-                <div> 
-                    {cartItems.map((itemReceived) => {
-                        return (
-                        <>
-                            <div key={itemReceived.id} className={CartCSS.itemContainer}>
-                                <img src={itemReceived.img} alt={itemReceived.name} className={CartCSS.itemImg}/>
-                                <div className={CartCSS.texts}>
-                                    <h2>{itemReceived.name}</h2>
-                                    <p>{itemReceived.quantity} x ${itemReceived.price}</p> 
-                                
-                                </div>
-                                <div onClick={()=>removeItem(itemReceived)} className={CartCSS.delete}>
-                                    <img src="../../imgs/DeleteIcon.png" alt="Delete"/>
-                                </div> 
-                            </div>
-                            <div>
-                                <hr/>
-                                <h2>Total a pagar: ${TotalPrice}</h2>
-                                <Form cartItems={cartItems}/>
-                            </div> 
-                        </>
-                    )}
-                )}
-            </div>
             }
         </div>
     )
