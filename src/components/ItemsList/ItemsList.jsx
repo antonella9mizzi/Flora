2b44e67b781aa1ad2a533c6f9ed40e963a1fc825
Antonella <contactamizzi@gmail.com>
2021-09-07 15:40:37 +0200

    ItemList issues fixed

--- src/components/ItemsList/ItemsList.jsx
+++ src/components/ItemsList/ItemsList.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Items from './Items';
-
+import  ItemsCSS from './Items.module.css';
+import { database } from '../../firebase/firebase';
+import { useParams } from "react-router-dom";
 
 const ItemsList = () =>{
+    const [productosMostrar, setProductosMostrar] = useState([]); 
+    const {categoryId} = useParams();
 
+    useEffect(() => {
+        setProductosMostrar([]);
+        const productos = database.collection("plantas");
+
+        if (categoryId) {
+        productos
+            .where("categoria", "==", categoryId)
+            .get()
+            .then((query) =>
+            setProductosMostrar(
+                query.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id };
+                })
+            )
+            );
+        } else {
+        productos.get().then((query) =>
+            setProductosMostrar(
+            query.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id };
+            })
+            )
+        );
+        }
+    }, [categoryId]);
+    
    
-    return(
-        <div>
-            <Items/>
-        </div>
-        
-        
-    );
+    return(       
+        <div className={ItemsCSS.displayItems}>
+           <div>
+            {/* {GetItems()} */}
+            </div>
+            {productosMostrar.map((producto) => (
+             <div>
+                <Items producto={producto} key={producto.id}/>
+            </div>            
+            ))}       
+        </div>     
+    )
      
 }
 export default ItemsList;
